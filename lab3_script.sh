225462f020e1ca91b74c2a153aeb63780cedc76e
tkleekamp <kleekamp@colorado.edu>
2019-09-26 11:19:48 -0600

    Some default message

--- lab3_script.sh
+++ lab3_script.sh
@@ -11,10 +11,10 @@ echo "Enter regex"
 read regex
 grep $regex $filename
 echo "Find the number of phone numbers listed below"
-grep -c '[0-9]\{3\}\-[0-9]\{3\}-[0-9]\{4\}' /Documents/lab_3/regex_practice.txt
+egrep "^[0-9]{3}-[0-9]{3}-[0-9]{4}$" regex_practice.txt -c
 echo "The number of emails in the file listed below"
-grep -E -c "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\.[A-Za-z]{2,6}\b" /Documents/lab_3/regex_practice.txt
+grep "@" regex_practice.txt -c
 echo "Phone numbers with 303 area code listed below"
-grep -o "303\-[0-9]\{3\}\-[0-9]\{4\}\b" /Documents/lab_3/regex_practice.txt
+egrep "303-[0-9]{3}-[0-9]{4}$" regex_practice.txt 
 echo "Append emails with @geocites with emails given"
-grep "@geocities.com" /Documents/lab_3/regex_practice.txt >> email_results.txt
+grep "@geocities.com" regex_practice.txt > email_results.txt
